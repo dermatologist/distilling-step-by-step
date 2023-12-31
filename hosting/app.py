2d299b4be4bc573cad40ccb9ea4b4dd393bc0bd8
Bell Eapen <github@gulfdoctor.net>
2023-12-31 17:06:18 -0500

    Update Dockerfile and app.py

--- hosting/app.py
+++ hosting/app.py
@@ -1,26 +1,33 @@
+import sys
+import logging
 from fastapi import FastAPI, APIRouter
 import uvicorn
-import logging
+from .base_server import BaseServer
+from .base_model import BaseModel
 
-logging.basicConfig(level = logging.INFO)
+_model: BaseModel = None
+_server: BaseServer = None
+# Set up logger
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+log = logging.getLogger(__name__)
 
 app = FastAPI()
 router = APIRouter()
 
 @router.get("/")
 async def home():
-  return {"message": "Machine Learning service"}
+    return {"message": "Machine Learning service"}
 
 @router.post("/predict")
 async def data(data: dict):
-    return {"message": "Data received"}
+    return _server.predict(data)
 
 @router.get("/health")
 async def health():
-  return {"message": "Model is healthy"}
+    return _server.health_check()
 
 
 app.include_router(router)
 
 if __name__ == "__main__":
-  uvicorn.run("app:app", reload=True, port=8080, host="0.0.0.0")
+    uvicorn.run("app:app", reload=True, port=8080, host="0.0.0.0")
--- hosting/base_server.py
+++ hosting/base_server.py
@@ -1,19 +1,47 @@
+import sys
+import logging
 from abc import ABC, abstractmethod
-
+from typing import Any
+from time import perf_counter
 from .base_model import BaseModel
-
+# Set up logger
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+log = logging.getLogger(__name__)
 
 class BaseServer(ABC):
 
     """A server class to lead the model and tokenizer"""
 
+    request_schema: Any = {
+    "type": "object",
+    "properties": {
+        "text": {"type": "string", "minLength": 1, "maxLength": 1000},
+        "labels": {
+            "type": "array",
+            "items": {"type": "string", "minLength": 1, "maxLength": 10},
+            "minItems": 1,
+        },
+    },
+        "required": ["text", "labels"],
+    }
+
     def __init__(self,
                 model: BaseModel,
+                request_schema: Any = None,
                  ) -> None:
-        self.model = model.load_model()
-        self.tokenizer = model.load_tokenizer()
+        self.model = model
+        if request_schema is not None:
+            self.request_schema = request_schema
+
 
     @abstractmethod
-    def serve(self):
-        pass
+    def health_check(self) -> Any:
+        """Health check endpoint"""
+        self.model.load()
+        return {"status": "ok"}
 
+
+    @abstractmethod
+    def predict(self, input: Any, **kwargs) -> Any:
+        result = self.model.predict(input)
+        return result
